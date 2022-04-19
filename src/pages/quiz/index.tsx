4816447896adabbbb8de8981a379556b71ca0528
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-19 13:10:27 +0600

    Quiz component added

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -2,7 +2,9 @@ import { Box, Typography } from "@mui/material"
 import QuizHome from "components/quiz/QuizHome";
 import Head from "next/head";
 
-const Quiz =()=>{
+const Quiz =({quizs})=>{
+    console.log(quizs)
+    
     return<> 
             <Head>
                 <title>Course Cloud || Quiz</title>
@@ -14,3 +16,16 @@ const Quiz =()=>{
 }
 
 export default Quiz;
+
+
+export async function  getStaticProps() {
+    const res = await fetch('https://tawsifhye.github.io/data/quizes.json')
+
+    const quizs= await res.json()
+
+    return{
+        props:{
+            quizs
+        }
+    }
+}
