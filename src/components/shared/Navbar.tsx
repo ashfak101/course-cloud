c3ea616a27ae6004e0694bb0eaa2bc5c35fd6ed4
Hamim <rahmanhamiminfo@gmail.com>
2022-04-19 16:38:59 +0600

    nav in progress

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,234 +1,80 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+ AppBar,
+ Box,
+ Button,
+ Container,
+ Divider,
+ IconButton,
+ InputBase,
+ Paper,
+ Toolbar,
+ Typography,
+} from "@mui/material";
+import Image from "next/image";
 import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
+import MenuIcon from "@mui/icons-material/Menu";
+import CommonButton from "./buttons/CommonButton";
+import Link from "next/link";
 
-const Search = styled("div")(({ theme }) => ({
- position: "relative",
- borderRadius: theme.shape.borderRadius,
- backgroundColor: alpha(theme.palette.common.white, 0.15),
- "&:hover": {
-  backgroundColor: alpha(theme.palette.common.white, 0.25),
- },
- marginRight: theme.spacing(2),
- marginLeft: 0,
- width: "100%",
- [theme.breakpoints.up("sm")]: {
-  marginLeft: theme.spacing(3),
-  width: "auto",
- },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
- padding: theme.spacing(0, 2),
- height: "100%",
- position: "absolute",
- pointerEvents: "none",
- display: "flex",
- alignItems: "center",
- justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
- color: "inherit",
- "& .MuiInputBase-input": {
-  padding: theme.spacing(1, 1, 1, 0),
-  // vertical padding + font size from searchIcon
-  paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  transition: theme.transitions.create("width"),
-  width: "100%",
-  [theme.breakpoints.up("md")]: {
-   width: "20ch",
-  },
- },
-}));
-
-export default function PrimarySearchAppBar() {
- const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
- const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-  React.useState<null | HTMLElement>(null);
-
- const isMenuOpen = Boolean(anchorEl);
- const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
- const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-  setAnchorEl(event.currentTarget);
- };
-
- const handleMobileMenuClose = () => {
-  setMobileMoreAnchorEl(null);
- };
-
- const handleMenuClose = () => {
-  setAnchorEl(null);
-  handleMobileMenuClose();
- };
-
- const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-  setMobileMoreAnchorEl(event.currentTarget);
- };
-
- const menuId = "primary-search-account-menu";
- const renderMenu = (
-  <Menu
-   anchorEl={anchorEl}
-   anchorOrigin={{
-    vertical: "top",
-    horizontal: "right",
-   }}
-   id={menuId}
-   keepMounted
-   transformOrigin={{
-    vertical: "top",
-    horizontal: "right",
-   }}
-   open={isMenuOpen}
-   onClose={handleMenuClose}
-  >
-   <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-   <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-  </Menu>
- );
-
- const mobileMenuId = "primary-search-account-menu-mobile";
- const renderMobileMenu = (
-  <Menu
-   anchorEl={mobileMoreAnchorEl}
-   anchorOrigin={{
-    vertical: "top",
-    horizontal: "right",
-   }}
-   id={mobileMenuId}
-   keepMounted
-   transformOrigin={{
-    vertical: "top",
-    horizontal: "right",
-   }}
-   open={isMobileMenuOpen}
-   onClose={handleMobileMenuClose}
-  >
-   <MenuItem>
-    <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-     <Badge badgeContent={4} color="error">
-      <MailIcon />
-     </Badge>
-    </IconButton>
-    <p>Messages</p>
-   </MenuItem>
-   <MenuItem>
-    <IconButton
-     size="large"
-     aria-label="show 17 new notifications"
-     color="inherit"
-    >
-     <Badge badgeContent={17} color="error">
-      <NotificationsIcon />
-     </Badge>
-    </IconButton>
-    <p>Notifications</p>
-   </MenuItem>
-   <MenuItem onClick={handleProfileMenuOpen}>
-    <IconButton
-     size="large"
-     aria-label="account of current user"
-     aria-controls="primary-search-account-menu"
-     aria-haspopup="true"
-     color="inherit"
-    >
-     <AccountCircle />
-    </IconButton>
-    <p>Profile</p>
-   </MenuItem>
-  </Menu>
- );
-
+const Navbar = () => {
  return (
-  <Box sx={{ flexGrow: 1 }}>
-   <AppBar position="static">
-    <Toolbar>
-     <IconButton
-      size="large"
-      edge="start"
-      color="inherit"
-      aria-label="open drawer"
-      sx={{ mr: 2 }}
-     >
-      <MenuIcon />
-     </IconButton>
+  <AppBar sx={{ bgcolor: "#21252D" }} position="static">
+   <Container maxWidth="xl">
+    <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
      <Typography
       variant="h6"
-      noWrap
       component="div"
-      sx={{ display: { xs: "none", sm: "block" } }}
+      sx={{ display: "flex", alignItems: "center" }}
      >
-      MUI
-     </Typography>
-     <Search>
-      <SearchIconWrapper>
-       <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-       placeholder="Search…"
-       inputProps={{ "aria-label": "search" }}
+      <Image
+       src="/assets/images/site-logo.png"
+       height="50px"
+       width="200px"
+       alt="logo"
+       objectFit={"contain"}
       />
-     </Search>
-     <Box sx={{ flexGrow: 1 }} />
-     <Box sx={{ display: { xs: "none", md: "flex" } }}>
-      <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-       <Badge badgeContent={4} color="error">
-        <MailIcon />
-       </Badge>
-      </IconButton>
-      <IconButton
-       size="large"
-       aria-label="show 17 new notifications"
-       color="inherit"
+      <Paper
+       component="form"
+       sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 220,
+        bgcolor: "#191C21",
+        border: ".5px solid #918E92",
+        ml: { md: 10, xs: 2 },
+       }}
       >
-       <Badge badgeContent={17} color="error">
-        <NotificationsIcon />
-       </Badge>
-      </IconButton>
-      <IconButton
-       size="large"
-       edge="end"
-       aria-label="account of current user"
-       aria-controls={menuId}
-       aria-haspopup="true"
-       onClick={handleProfileMenuOpen}
-       color="inherit"
-      >
-       <AccountCircle />
-      </IconButton>
+       <InputBase
+        sx={{ ml: 1, flex: 1, color: "#fff" }}
+        placeholder="Search"
+        inputProps={{ "aria-label": "search google maps" }}
+       />
+       <IconButton
+        type="submit"
+        sx={{ p: "5px", color: "#fff" }}
+        aria-label="search"
+       >
+        <SearchIcon />
+       </IconButton>
+      </Paper>
+     </Typography>
+     <Box>
+      <Link href="/">
+       <a style={{ margin: "0px 4px 0px 4px" }}> CATEGORIES </a>
+      </Link>
+      <Link href="/">
+       <a style={{ margin: "0px 4px 0px 4px" }}> FREE QUIZ </a>
+      </Link>
+      <Link href="/">
+       <a style={{ margin: "0px 4px 0px 4px" }}> SUPPORT </a>
+      </Link>
      </Box>
-     <Box sx={{ display: { xs: "flex", md: "none" } }}>
-      <IconButton
-       size="large"
-       aria-label="show more"
-       aria-controls={mobileMenuId}
-       aria-haspopup="true"
-       onClick={handleMobileMenuOpen}
-       color="inherit"
-      >
-       <MoreIcon />
-      </IconButton>
-     </Box>
+     <CommonButton textValue="Login" />
     </Toolbar>
-   </AppBar>
-   {renderMobileMenu}
-   {renderMenu}
-  </Box>
+   </Container>
+  </AppBar>
  );
-}
+};
+
+export default Navbar;
