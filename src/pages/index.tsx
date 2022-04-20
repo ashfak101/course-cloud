8495f9617aefdb5b32283b1e00761d808a0674bc
Hamim <rahmanhamiminfo@gmail.com>
2022-04-20 13:43:31 +0600

    home to name changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Banner from "components/home/Banner";
 import BecomeInstructor from "components/home/BecomeInstructor";
-import ExploreCourses from "components/Home/ExploreCourses";
+import ExploreCourses from "components/home/ExploreCourses";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
