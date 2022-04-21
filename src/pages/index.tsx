b091306a30b9a825ae8569e0cd9e9547f674381c
Hamim <rahmanhamiminfo@gmail.com>
2022-04-21 12:01:11 +0600

    explore course

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,18 @@ import EasyAccess from "components/home/EasyAccess";
 import Faq from "components/home/Faq";
 import Subscription from "components/home/Subscription";
 import ExploreCourses from "components/home/ExploreCourses";
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Footer from "components/home/Footer";
+import { CourseData, GetCoursesData } from "../../types";
 
-const Home: NextPage = ({ courses }: any) => {
+const Home: NextPage<{ courses: CourseData }> = ({ courses }) => {
  console.log(courses);
 
  return (
   <>
    <Banner />
    <BecomeInstructor />
-   <ExploreCourses />
+   <ExploreCourses courses={courses} />
    <EasyAccess />
    <Subscription />
    <Faq />
@@ -25,15 +26,15 @@ const Home: NextPage = ({ courses }: any) => {
 
 export default Home;
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (context) => {
  // https://tawsifhye.github.io/data/courses.json
 
  const courseResponse = await fetch("http://localhost:3000/api/course-data");
- const courses = await courseResponse.json();
+ const courses: GetCoursesData = await courseResponse.json();
 
  return {
   props: {
    courses,
   },
  };
-}
+};
