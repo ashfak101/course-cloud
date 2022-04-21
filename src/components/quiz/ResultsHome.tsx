1215e13bad38a74d64c1ace6f9756388cf9d5861
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-21 15:33:07 +0600

    Result Page Added

--- src/components/quiz/ResultsHome.tsx
+++ src/components/quiz/ResultsHome.tsx
@@ -1,10 +1,34 @@
+import Quiz from "models/quizModels";
+import { useState } from "react";
 import { useSelector } from "react-redux"
 import { State } from "redux/reducers";
 
+type AnswerArray={
+    questionId:string;
+    question:string;
+    options:{}[];
+    selectedAnswer:{};
+    level:string
+
+}
 const ResultsHome = () => {
     
-    const allAnswer= useSelector((state:State)=>state.allAnswer)
-    console.log(allAnswer);
+    const answer= useSelector((state:State)=>state.allAnswer.finalAnswer)
+    const [level,setLevel]=useState('')
+    let beginner:number = 0;
+    let intermediate:number = 0;
+    let advanced:number = 0;
+    let correctAnswerArray=[];
+    console.log(Array(answer));
+    
+    answer?.forEach((element)=>{
+            element.options.forEach((option)=>{
+                if(option.isCorrect && element.selectedAnswer.isCorrect){
+                    correctAnswerArray.push(option.option)
+                    // quizAnswerlevel.push(element.level)
+                }
+            })
+    })
     
   return (
     <div>ResultsHome</div>
--- src/redux/reducers/quizReducer.ts
+++ src/redux/reducers/quizReducer.ts
@@ -1,15 +1,21 @@
+import { type } from "os";
 
+type Option ={
+    id:string;
+    option:string;
+    isCorrect:boolean;
+}
 
 type Quiz ={
     questioonId:string;
     question:string;
-    options:{}[];
-    selectedAnswer:{};
+    options:Option[];
+    selectedAnswer:Option;
     level:string
 }
 
 interface QuizState {
-    finalAnswer:Quiz[] |null
+    finalAnswer:Quiz[]|null
 }
 const initialState ={
   
@@ -19,7 +25,7 @@ type Action ={
     type:string;
     payload:Quiz[]
 }
-export const quizReducer=(state:QuizState =initialState,action:Action):QuizState=>{
+export const quizReducer=(state:QuizState=initialState,action:Action):QuizState=>{
         switch(action.type){
             case  "SUBMIT_QUIZ":
                 return{
