1072f0077d366362af115f3736a38babf5073113
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-21 16:20:02 +0600

    .

--- src/components/quiz/QuizResultHeader.tsx
+++ src/components/quiz/QuizResultHeader.tsx
@@ -19,7 +19,7 @@ type Props ={
 const QuizResultHeader = (props:Props) => {
     const {answer,correctAnswerArray}=props
   return (
-    <Box sx={{background: "linear-gradient(180deg, rgba(255, 255, 255, 0.25) 51.22%, rgba(255, 255, 255, 0) 127.72%), #ED3B45",px:2,color:"#fff", height:"50px", display: 'flex',justifyContent: 'space-between',alignItems: 'center',borderTopRightRadius:"10px",borderTopLeftRadius:"10px"}}>
+    <Box sx={{background: "linear-gradient(180deg, rgba(255, 255, 255, 0.25) 51.22%, rgba(255, 255, 255, 0) 127.72%), #ED3B45",px:2,mt:2,color:"#fff", height:"50px", display: 'flex',justifyContent: 'space-between',alignItems: 'center',borderTopRightRadius:"10px",borderTopLeftRadius:"10px"}}>
        <Typography>{correctAnswerArray.length} /{ answer.length} answer</Typography>
 
        { 
