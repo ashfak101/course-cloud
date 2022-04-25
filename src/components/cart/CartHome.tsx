ae785c438079c62f76557c72d6ddff2ec1592f7f
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-25 12:37:20 +0600

    Cart Components

---
+++ src/components/cart/CartHome.tsx
@@ -0,0 +1,16 @@
+import { Container, Box, Typography } from "@mui/material"
+
+
+const CartHome = () => {
+    return (
+        <Box sx={{ height: "211px", background: "#191C21" }}>
+            <Container sx={{py:9}}>
+                <Typography sx={{color:"#B2ADAD"}}>Home / MyCart</Typography>
+                <Typography sx={{color:"#FBF4F4",fontFamily:"Lato",fontWeight:"700",fontSize:"30px"}}>MyCart</Typography>
+
+            </Container>
+        </Box>
+    )
+}
+
+export default CartHome
