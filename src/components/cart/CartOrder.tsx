6951d78e42ec3a52520a68a717e80952495c6bdd
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-26 12:45:32 +0600

    Cupon button

--- src/components/cart/CartOrder.tsx
+++ src/components/cart/CartOrder.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Container, Typography } from '@mui/material';
+import { Button, Container, TextField, Typography } from '@mui/material';
 import Image from 'next/image';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -13,7 +13,25 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
-
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#333',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#999',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'linear-gradient(to right, #232526 0%, #414340  56%, #232526  100%)',
+    },
+    '&:hover fieldset': {
+      borderColor: 'linear-gradient(to right, #232526 0%, #414340  76%, #232526  100%)',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#333',
+    },
+  },
+});
 const CartOrder = () => {
   return (
     <Box sx={{ mt: 10 }}>
@@ -53,21 +71,32 @@ const CartOrder = () => {
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>Product</Typography>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>Subtotal</Typography>
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4',borderBottom:'0.568px solid #C6C6C6',pb:2 }}>
-                  <Typography sx={{ fontWeight: '600', fontSize: '20px',width:'350px' ,textAlign:'left'}}>Certificate in British Sign Language
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4', borderBottom: '0.568px solid #C6C6C6', pb: 2 }}>
+                  <Typography sx={{ fontWeight: '600', fontSize: '20px', width: '350px', textAlign: 'left' }}>Certificate in British Sign Language
                     (BSL) Level 1 & 2</Typography>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px', }}>	£15.00</Typography>
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4', borderBottom:'0.568px solid #C6C6C6',pb:2}}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4', borderBottom: '0.568px solid #C6C6C6', pb: 2 }}>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>Subtotal</Typography>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>	£35.00</Typography>
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4',borderBottom:'0.568px solid #C6C6C6',pb:2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 4, color: '#FBF4F4', borderBottom: '0.568px solid #C6C6C6', pb: 2 }}>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>Total</Typography>
                   <Typography sx={{ fontWeight: '600', fontSize: '20px' }}>	£35.00</Typography>
                 </Box>
                 <Box>
-                  
+                  <form>
+                    <CssTextField sx={{ background: "#fff", borderRadius: '5px' }} id="outlined-basic" placeholder='Enter Cupon' variant="outlined" />
+                    <Button type='submit' variant='contained' sx={{
+                      fontSize: '15px'
+                      , color: '#191C21', 
+                      bgcolor: '#D19F28', p: "15px 25px", borderRadius: '0 5px 5px 0',
+                       ml: '-2px',
+                       "&:hover":{
+                          bgcolor:'#FBDE44'
+                       }
+                    }}>Apply</Button>
+                  </form>
                 </Box>
               </Item>
             </Grid>
