e72159df7ab5744f9675d6708df42516172f0bed
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-26 11:15:27 +0600

    Cart order design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,47 +11,48 @@ import DealoftheDay from "components/home/DealoftheDay";
 import FeaturedInstructors from "components/home/FeaturedInstructors";
 import Feedback from "components/home/Feedback";
 import { GetBlogsData } from "../../types-blog";
+import RecentBlogs from "components/home/RecentBlogs";
 
 const Home: NextPage<{ courses: CourseData }> = ({ courses }) => {
- return (
-  <>
-   <Banner />
-   <BecomeInstructor />
-   <ExploreCourses courses={courses} />
-   <DealoftheDay courses={courses} />
-   <FeaturedInstructors courses={courses} />
-   <Feedback courses={courses} />
-   <RecentBlogs />
-   <EasyAccess />
-   <Subscription />
-   <Faq />
-   <Footer />
-  </>
- );
+    return (
+        <>
+            <Banner />
+            <BecomeInstructor />
+            <ExploreCourses courses={courses} />
+            <DealoftheDay courses={courses} />
+            <FeaturedInstructors courses={courses} />
+            <Feedback courses={courses} />
+            <RecentBlogs />
+            <EasyAccess />
+            <Subscription />
+            <Faq />
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
 
 export const getStaticProps: GetStaticProps = async (context) => {
- const courseResponse = await fetch(
-  "https://api.npoint.io/8b635b31d3c9d683fcec"
- );
+    const courseResponse = await fetch(
+        "https://api.npoint.io/8b635b31d3c9d683fcec"
+    );
 
- const blogResponse = await fetch(
-    "https://api.npoint.io/25e8205992894fabbd1d"
-);
-const blogs: GetBlogsData = await blogResponse.json();
+    const blogResponse = await fetch(
+        "https://api.npoint.io/25e8205992894fabbd1d"
+    );
+    const blogs: GetBlogsData = await blogResponse.json();
 
 
 
- const courses: GetCoursesData = await courseResponse.json();
+    const courses: GetCoursesData = await courseResponse.json();
 
- return {
-  props: {
-   courses,
-   blogs
-  },
- };
+    return {
+        props: {
+            courses,
+            blogs
+        },
+    };
 };
 
 
