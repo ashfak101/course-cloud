3e47a3b416821d44c806cc4fe6795e4d413890f8
Hamdan Ahmed <45794213+MrHamdan@users.noreply.github.com>
2022-04-27 10:33:25 +0600

    blog detail page design finished

--- src/components/blogs/BlogPageCard.tsx
+++ src/components/blogs/BlogPageCard.tsx
@@ -6,83 +6,83 @@ import { BlogsData } from "../../../types-blog";
 import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
 
 type Props = {
- blogs: BlogsData[];
+    blogs: BlogsData[];
 };
 
 const BlogPageCard = ({ blogs }: Props) => {
- return (
-  <Container sx={{ my: 10, display: "flex", justifyContent: "center" }}>
-   <Box sx={{ bgcolor: "#191C23", p: 4, width: { xs: "440px", md: "100%" } }}>
-    <Grid container spacing={2} sx={{ alignItems: "center" }}>
-     <Grid item xs={12} md={4}>
-      <Image
-       src={blogs[0].img}
-       width="366px"
-       height="255px"
-       alt=""
-       objectFit="cover"
-      />
-     </Grid>
-     <Grid item xs={12} md={8} sx={{}}>
-      <Box
-       sx={{
-        display: { xs: "block", md: "flex" },
-        justifyContent: "space-between",
-        alignItems: "center",
-       }}
-      >
-       <Typography sx={{ mr: 1 }} variant="h5">
-        {blogs[0].title}
-       </Typography>
-       <Typography
-        sx={{
-         width: "fit-content",
-         textAlign: "center",
-         mx: "auto",
-         px: "10px",
-         py: "5px",
-         bgcolor: "rgba(251, 222, 68, 0.3)",
-         borderRadius: "5px",
-         color: "rgb(251, 222, 68)",
-        }}
-       >
-        {blogs[0].publishDate}
-       </Typography>
-      </Box>
-      <Box sx={{ my: 2, display: "flex", alignItems: "center" }}>
-       <Typography sx={{ color: "#8B8787" }}>{blogs[0].publishDate}</Typography>
-       <Box sx={{ display: "flex", alignItems: "center", ml: 5 }}>
-        <Image
-         src="/assets/images/instructor-img.png"
-         width="30px"
-         height="30px"
-         alt=""
-         objectFit="cover"
-        />
-        <Typography sx={{ mx: 1 }}>
-         <Typography sx={{ color: "#8B8787" }} component="span">
-          By
-         </Typography>
-         Alan Bell
-        </Typography>
-       </Box>
-      </Box>
-      <Box>
-       <Typography sx={{ my: 2, color: "#8B8787" }}>
-        {blogs[0].startingSection.slice(0, 300)} ...
-       </Typography>
-      </Box>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-       <Button>Read More</Button>
-       <Button>
-        <BookmarkAddIcon />
-       </Button>
-      </Box>
-     </Grid>
-    </Grid>
-   </Box>
-  </Container>
- );
+    return (
+        <Container sx={{ my: 10, display: "flex", justifyContent: "center" }}>
+            <Box sx={{ bgcolor: "#191C23", p: 4, width: { xs: "440px", md: "100%" } }}>
+                <Grid container spacing={2} sx={{ alignItems: "center" }}>
+                    <Grid item xs={12} md={4}>
+                        <Image
+                            src={blogs[0].img}
+                            width="366px"
+                            height="255px"
+                            alt=""
+                            objectFit="cover"
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={8} sx={{}}>
+                        <Box
+                            sx={{
+                                display: { xs: "block", md: "flex" },
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography sx={{ mr: 1 }} variant="h5">
+                                {blogs[0].title}
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    width: "fit-content",
+                                    textAlign: "center",
+                                    mx: "auto",
+                                    px: "10px",
+                                    py: "5px",
+                                    bgcolor: "rgba(251, 222, 68, 0.3)",
+                                    borderRadius: "5px",
+                                    color: "rgb(251, 222, 68)",
+                                }}
+                            >
+                                {blogs[0].publishDate}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ my: 2, display: "flex", alignItems: "center" }}>
+                            <Typography sx={{ color: "#8B8787" }}>{blogs[0].publishDate}</Typography>
+                            <Box sx={{ display: "flex", alignItems: "center", ml: 5 }}>
+                                <Image
+                                    src="/assets/images/instructor-img.png"
+                                    width="30px"
+                                    height="30px"
+                                    alt=""
+                                    objectFit="cover"
+                                />
+                                <Typography sx={{ mx: 1 }}>
+                                    <Typography sx={{ color: "#8B8787" }} component="span">
+                                        By
+                                    </Typography>
+                                    Alan Bell
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box>
+                            <Typography sx={{ my: 2, color: "#8B8787" }}>
+                                {blogs[0].startingSection.slice(0, 300)} ...
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                            <Button>Read More</Button>
+                            <Button>
+                                <Image src="/assets/images/saved.png" width="21px" height="26px" alt="" />
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    );
 };
 
 export default BlogPageCard;
