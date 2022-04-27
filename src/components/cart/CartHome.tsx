efdbee74d01caea550811a146da97afd35f50215
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-27 10:31:38 +0600

    Stepper added

--- src/components/cart/CartHome.tsx
+++ src/components/cart/CartHome.tsx
@@ -1,4 +1,5 @@
-import { Container, Box, Typography } from "@mui/material"
+import { Container, Box, Typography, Button, Step, StepLabel, Stepper } from "@mui/material"
+import React, { useState } from "react";
 import CartOrder from "./CartOrder"
 import Checkout from "./Checkout";
 const Styles = {
@@ -12,19 +13,58 @@ const Styles = {
   },
 };
 
-
+const steps = ['Cart', 'Billing Details', 'Completed'];
 const CartHome = () => {
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const [skipped, setSkipped] = useState(new Set());
+
+  const isStepOptional = (step:number) => {
+    return step === 1
+  }
+  const isStepSkipped = (step:number) => {
+    return skipped.has(step);
+  };
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
   return (
     <>
       <Box sx={{ ...Styles.cartBackground }}>
         <Container maxWidth="xl" sx={{ py: 9 }}>
+
           <Typography sx={{ color: "#B2ADAD" }}>Home / MyCart</Typography>
           <Typography sx={{ color: "#FBF4F4", fontFamily: "Lato", fontWeight: "700", fontSize: "30px" }}>MyCart</Typography>
 
         </Container>
       </Box>
-      {/* <CartOrder></CartOrder> */}
-      <Checkout/>
+      <Container maxWidth='xl'>
+        <Box sx={{ width: '100%' }}>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+        </Box>
+      </Container >
+      {activeStep === steps.length-3 && <CartOrder></CartOrder>}
+     {activeStep === steps.length-2 &&  <Checkout />}
     </>
   )
 }
