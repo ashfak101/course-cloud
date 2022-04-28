b63c166b42f0a673f5c903ce33897abc05d5ee2a
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-04-28 10:48:36 +0600

    Course API changed

--- src/pages/blogs/[details]/index.tsx
+++ src/pages/blogs/[details]/index.tsx
@@ -8,51 +8,51 @@ import BlogDetails from "components/blogdetails/BlogDetails";
 import { GetCoursesData } from "../../../../types";
 
 const Item = styled(Paper)(({ theme }) => ({
- backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
- ...theme.typography.body2,
- padding: theme.spacing(1),
- textAlign: "center",
- color: theme.palette.text.secondary,
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
 }));
 
 const BlogDetail = ({ blog, allBlogsData, courses }: any) => {
- return (
-  <>
-   <BlogDetails blog={blog} allBlogsData={allBlogsData} courses={courses} />
-  </>
- );
+    return (
+        <>
+            <BlogDetails blog={blog} allBlogsData={allBlogsData} courses={courses} />
+        </>
+    );
 };
 export default BlogDetail;
 
 export async function getStaticPaths() {
- const res = await fetch(
-  "https://tawsifhye.github.io/data/coursecloudblogs.json"
- );
- const blogs = await res.json();
- const paths = blogs.map((blog: any) => ({
-  params: { details: blog.id.toString() },
- }));
- return { paths, fallback: false };
+    const res = await fetch(
+        "https://tawsifhye.github.io/data/coursecloudblogs.json"
+    );
+    const blogs = await res.json();
+    const paths = blogs.map((blog: any) => ({
+        params: { details: blog.id.toString() },
+    }));
+    return { paths, fallback: false };
 }
 
 export async function getStaticProps(context: any) {
- const res = await fetch(
-  "https://tawsifhye.github.io/data/coursecloudblogs.json"
- );
- const blogs = await res.json();
- const blog = blogs.find(
-  (blog: any) => blog.id.toString() === context.params.details
- );
+    const res = await fetch(
+        "https://tawsifhye.github.io/data/coursecloudblogs.json"
+    );
+    const blogs = await res.json();
+    const blog = blogs.find(
+        (blog: any) => blog.id.toString() === context.params.details
+    );
 
- const allBlogsRes = await fetch(
-  "https://tawsifhye.github.io/data/coursecloudblogs.json"
- );
- const allBlogsData = await allBlogsRes.json();
+    const allBlogsRes = await fetch(
+        "https://tawsifhye.github.io/data/coursecloudblogs.json"
+    );
+    const allBlogsData = await allBlogsRes.json();
 
- const courseResponse = await fetch(
-  "https://api.npoint.io/8b635b31d3c9d683fcec"
- );
- const courses: GetCoursesData = await courseResponse.json();
+    const courseResponse = await fetch(
+        "https://tawsifhye.github.io/data/coursecloudcourses.json"
+    );
+    const courses: GetCoursesData = await courseResponse.json();
 
- return { props: { blog, allBlogsData, courses } };
+    return { props: { blog, allBlogsData, courses } };
 }
