da76c2cce237a6855c49ae49159afac3405cd0f1
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-29 10:31:17 +0600

    quiz type fixed

--- src/components/quiz/QuizHome.tsx
+++ src/components/quiz/QuizHome.tsx
@@ -21,6 +21,7 @@ type Ans = {
   selectedAnswer: {};
   level: string;
 };
+type  Time = number | string;
 
 const QuizHome = (props: QuizProps) => {
   const quizs = props.quizs.slice(0, 10);
@@ -100,10 +101,10 @@ const QuizHome = (props: QuizProps) => {
   // Timer Function Start ------------------------------------------------------
   // ---------------------------------------------------------------------------
   const [time, setTime] = useState<number>(300);
-  const [timeString, setTimeString] = useState<any>();
+  const [timeString, setTimeString] = useState<Time>();
 
-  const [eachQuizTimer, setEachQuizTimer] = useState<number>(3);
-  const [eachQuizTimerString, setEachQuizTimerString] = useState<any>();
+  const [eachQuizTimer, setEachQuizTimer] = useState<number>(30);
+  const [eachQuizTimerString, setEachQuizTimerString] = useState<Time>();
 
   let hours = Math.floor(time / 3600); // get hours
   let minutes = Math.floor((time - hours * 3600) / 60); // get minutes
@@ -114,7 +115,8 @@ const QuizHome = (props: QuizProps) => {
   let minutesEachQuiz = Math.floor((eachQuizTimer - hoursEachQuiz * 3600) / 60); // get minutes
   let secondsEachQuiz = eachQuizTimer - hoursEachQuiz * 3600 - minutesEachQuiz * 60; //  get seconds
 
-  function convTime(hours: any, minutes: any, seconds: any) {
+  
+  function convTime(hours: Time, minutes: Time, seconds: Time) {
     if (hours < 10) {
       hours = "0" + hours;
     }
@@ -131,7 +133,7 @@ const QuizHome = (props: QuizProps) => {
 
 
   // Function for each quiz timer
-  function convTimeEachQuiz(hours: any, minutes: any, seconds: any) {
+  function convTimeEachQuiz(hours: Time, minutes: Time, seconds: Time) {
     if (hours < 10) {
       hours = "0" + hours;
     }
@@ -151,12 +153,12 @@ const QuizHome = (props: QuizProps) => {
         setTime((prevTime) => prevTime - 1);
       }, 1000);
     } else {
-      
+
       if (index === quizs.length - 1) {
         submitQuiz();
       } else {
         goNext();
-        setEachQuizTimer(3);
+        setEachQuizTimer(30);
       }
     }
 
